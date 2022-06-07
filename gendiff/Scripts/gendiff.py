7a71f02a58d8dcc7bbdb66b0a4d46ab9d1f7663e
pavelpminaev <pavelpminaev@gmail.com>
2022-06-07 23:25:03 +0300

    def main with argparse module parametrs

--- gendiff/Scripts/gendiff.py
+++ gendiff/Scripts/gendiff.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 """Script....."""
 
+import argparse
+
 
 def main():
     """..."""
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+
+    parser.add_argument('first_file', type=str, help='first file for comparison')
+    parser.add_argument('second_file', type=str, help='second file for comparison')
+
+    args = parser.parse_args()
+    print(args.difference(args.first_file, args.second_file))
+
 
 if __name__ == '__main__':
     main()
-
