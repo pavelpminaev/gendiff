cd4afe953ff6fd329a1633876d90fdb297e6d791
pavelpminaev <pavelpminaev@gmail.com>
2023-02-08 01:31:37 +0600

    add formatter for return diff_dict

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,29 +1,32 @@
 """Modul search difference between two collections"""
 
 from gendiff.parsing import get_data
+from formaters.all_formaters import format_change
+
+ERROR_MESSAGE = "Impossible to build difference. Check your files."
 
 
 def make_diff(node1, node2):
-
     def get_difference(key):
 
         if key in deleted_keys:
-            val = {'type': 'deleted', 'value': node1[key]}
+            value = {'type': 'deleted', 'value': node1[key]}
 
         elif key in added_keys:
-            val = {'type': 'added', 'value': node2[key]}
+            value = {'type': 'added', 'value': node2[key]}
 
         elif key in changed_keys and node1[key] == node2[key]:
-            val = {'type': 'unchanged', 'value': node1[key]}
+            value = {'type': 'unchanged', 'value': node1[key]}
 
         elif key in changed_keys and node1[key] != node2[key]:
             if isinstance(node1[key], dict) and isinstance(node2[key], dict):
-                val = {'type': 'internal_change',
-                       'value': make_diff(node1[key], node2[key])}
+                value = {'type': 'internal_change',
+                         'value': make_diff(node1[key], node2[key])}
             else:
-                val = {'type': 'changed_value',
-                       'value': [node1[key], node2[key]]}
-        return key, val
+                value = {'type': 'changed_value',
+                         'value': [node1[key], node2[key]]}
+
+        return {key, value}
 
     all_keys = sorted(set.union(set(node1), set(node2)))
     deleted_keys = set(node1).difference(set(node2))
@@ -33,19 +36,15 @@ def make_diff(node1, node2):
     return dict(map(get_difference, all_keys))
 
 
-def generate_diff(pathfile1, pathfile2):
+def generate_diff(path_first_file, path_second_file, formatter='stylish'):
+    data_of_first_file, data_of_second_file = \
+        get_data(path_first_file), get_data(path_second_file)
 
-    data1, data2 = get_data(pathfile1), get_data(pathfile2)
+    if (data_of_first_file is False) or (data_of_second_file is False):
+        return ERROR_MESSAGE
 
-    if (data1 is False) or (data2 is False):
-        return 'ERROR_MESSAGE'
+    diff_dict = make_diff(data_of_first_file, data_of_second_file)
 
-    diff_dict = make_diff(data1, data2)
+    format_style = format_change(formatter)
 
-    return diff_dict
-
-
-pathfile1 = '/Users/pavelminaev/python-project-lvl2/tests/fixtures/tree_files/tree1.json'
-pathfile2 = '/Users/pavelminaev/python-project-lvl2/tests/fixtures/tree_files/tree2.json'
-
-print(generate_diff(pathfile1, pathfile2))
+    return format_style(diff_dict)
