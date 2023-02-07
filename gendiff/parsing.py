0a04bcfe59f58f44c0b0db6eaad7fa10d4088159
pavelpminaev <pavelpminaev@gmail.com>
2023-02-07 14:27:15 +0600

    refactor annotation

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,49 +1,46 @@
+"""Module for parsing data from .yml and .json files.
+"""
+import _io
 import os
 import json
 import yaml
 
 
-
-"""
-Function get ending of file
-example: from 'gendiff/tree1.json' get '.json'.
-"""
-
-
-def get_ending(pathfile):
+def get_ending(pathfile: str) -> str:
+    """
+    Function get ending of file
+    example: from 'gendiff/tree1.json' get '.json'.
+    """
     return os.path.splitext(pathfile)[1]
 
 
-"""
-Open file and get parsing_data.
-"""
-
-
-def get_data(pathfile):
+def get_data(pathfile: str) -> str:
+    """
+    Open file and get parsing_data.
+    """
     with open(pathfile, 'r') as data:
+        print(type(data))
         return parsing_data(data, get_ending(pathfile))
 
 
-def parsing_data(data, ending):
+def parsing_data(data: _io.TextIOWrapper, ending: str) -> dict:
+    """
+    Checking opening file and return dictionary
+    with data of .json or .yml file
+    """
     try:
         if ending == '.yaml' or '.yml':
             data_dict = yaml.load(data, Loader=yaml.FullLoader)
         elif ending == '.json':
             data_dict = json.load(data)
-
         if data_dict is None:
-            raise TypeError     # поднять TypeError
+            raise TypeError  # поднять TypeError
     except (TypeError, yaml.parser.ParserError):
         return False
     else:
         return data_dict
 
 
-
 # variable for manual checking
-
-
-"""pathfile2 = '/Users/pavelminaev/python-project-lvl2/tests/fixtures/tree_files/tree1.json'
-
-
-print(get_data(pathfile2))"""
+pathfile2 = '/Users/pavelminaev/python-project-lvl2/tests/fixtures/tree_files/tree1.json'
+print(type(get_data(pathfile2)))
